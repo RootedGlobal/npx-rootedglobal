07b337e7725d768a13c6f5fe395c762cfe84db9f
Narain Sagar <narainmenghwar@gmail.com>
2020-04-24 03:55:03 +0200

    created my own card template

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 #!/usr/bin/env node
 
-const {
-  white,
-  bold,
-  blueBright
-} = require("colorette");
+const { white, bold, blueBright } = require("colorette");
 
-const work = white(`Software Architect @ ${blueBright('Modus Create')}`)
-const gde = `${blueBright('  Google Developers Expert')} ${white('in Angular')}`
-const twitter = blueBright('https://twitter.com/ahsan_ayz')
-const linkedIn = blueBright('https://linkedin.com/in/ahsanayaz')
-const github = blueBright('https://github.com/ahsanayaz')
-const slides = blueBright('https://slides.com/ahsanayaz')
-const medium = blueBright('https://medium.com/@ahsan.ayaz')
-const npx = white('npx ahsanayaz')
+const work = white(
+  `Full-stack software engineer & Tech Lead @ ${blueBright("Rooted Global")}`
+);
+const tagline = `${blueBright("  All things Web & JS")} ${white(
+  "(MEAN, MERN & LAMP tech-stack)"
+)}`;
+const website = blueBright("https://narainsagar.com");
+const twitter = blueBright("https://twitter.com/narainsagar");
+const linkedIn = blueBright("https://linkedin.com/in/narainsagar");
+const github = blueBright("https://github.com/narainsagar");
+const upwork = blueBright("https://upwork.com/freelancers/~01050a14629af3bda8");
+const medium = blueBright("https://medium.com/@narainsagar");
+const facebook = blueBright("https://fb.com/NarainSagarPage");
+const npx = white("npx narainsagar");
 
-const newline = '\n'
-const heading = `${white('  Muhammad Ahsan Ayaz /')} ${blueBright('ahsanayaz')}`
-const working = `${white(bold('     Work:'))}  ${work}`
-const linkedining = `${white(bold('  LinkedIn:'))} ${linkedIn}`
-const twittering = `${white(bold('  Twitter:'))}  ${twitter}`
-const githubing = `${white(bold('   GitHub:'))}  ${github}`
-const slidesing = `${white(bold('   Slides:'))}  ${slides}`;
-const mediuming = `${white(bold('   Medium:'))}  ${medium}`;
-const carding = `${white(bold('     Card:'))}  ${npx}`
+const newline = "\n";
+const heading = `${white("  Narain Menghwar /")} ${blueBright("narainsagar")}`;
+const websiteing = `${white(bold("  Website:"))}  ${website}`;
+const working = `${white(bold("     Work:"))}  ${work}`;
+const linkedining = `${white(bold(" LinkedIn:"))} ${linkedIn}`;
+const twittering = `${white(bold("  Twitter:"))}  ${twitter}`;
+const githubing = `${white(bold("   GitHub:"))}  ${github}`;
+const mediuming = `${white(bold("   Medium:"))}  ${medium}`;
+const upworking = `${white(bold("   Upwork:"))}  ${upwork}`;
+const facebooking = `${white(bold(" Facebook:"))}  ${facebook}`;
+const carding = `${white(bold("     Card:"))}  ${npx}`;
 
-let output = `${newline} ${newline} ${heading} ${newline} ${gde} ${newline} ${newline} ${working} ${newline} ${twittering} ${newline} ${linkedining} ${newline} ${githubing} ${newline} ${slidesing} ${newline} ${mediuming} ${newline} ${newline} ${carding} ${newline} ${newline}`;
+const output = `${newline} ${newline} ${heading} ${newline} ${tagline} ${newline} ${newline} ${working} ${newline} ${websiteing} ${newline} ${twittering} ${newline} ${linkedining} ${newline} ${githubing} ${newline} ${mediuming} ${newline} ${facebooking} ${newline} ${upworking} ${newline} ${newline} ${carding} ${newline} ${newline}`;
 
-console.log(output)
+console.log(output);
